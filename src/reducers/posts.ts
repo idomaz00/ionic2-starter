033a73e3b365360e691d7b8c92435504d470894b
Josipa Braica <josipa@dump.hr>
2017-02-11 15:22:39 +0100

    implementing store in progress

---
+++ src/reducers/posts.ts
@@ -0,0 +1,44 @@
+import { Post } from '../models/post';
+import * as posts from '../actions/posts';
+
+export interface State {
+    loaded: boolean;
+    loading: boolean;
+    posts: Post[];
+};
+
+const initialState: State = {
+    loaded: false,
+    loading: false,
+    posts: []
+};
+
+export function reducer(state = initialState, action: posts.Actions): State {
+    switch(action.type) {
+        case posts.ActionTypes.FETCH: {
+            return Object.assign({}, state, {
+                loading: true
+            });
+        }
+
+        case posts.ActionTypes.FETCH_SUCCESS: {
+            const newPosts = action.payload;
+
+            return Object.assign({}, state, {
+                loaded: true,
+                loading: false,
+                posts: [...state.posts, ...newPosts]
+            });
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
+
+export const getLoaded = (state: State) => state.loaded;
+
+export const getLoading = (state: State) => state.loading;
+
+export const getPosts = (state: State) => state.posts;
---
+++ src/services/posts.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Post } from '../models/post';
+
+@Injectable()
+export class PostsService {
+    private API_PATH: string = 'https://jsonplaceholder.typicode.com/posts';
+
+    constructor(private http: Http) {}
+
+    getPosts(): Observable<Post[]> {
+        return this.http.get(this.API_PATH).map(res => res.json());
+    }
+}
