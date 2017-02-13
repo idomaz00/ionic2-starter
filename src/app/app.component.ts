c79c33bed9f726a5df226658b24bb6adfc4c4b46
Izabela Domazet <idomaz00@fesb.hr>
2017-02-13 14:48:46 +0100

    some refactoring storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,19 +15,6 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-      let db = new SQLite();
-            db.openDatabase({
-                name: "data.db",
-                location: "default"
-            }).then(() => {
-                db.executeSql("CREATE TABLE IF NOT EXISTS favourites (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT)", {}).then((data) => {
-                    console.log("TABLE CREATED: ", data);
-                }, (error) => {
-                    console.error("Unable to execute sql", error);
-                })
-            }, (error) => {
-                console.error("Unable to open database", error);
-            });
       Splashscreen.hide();
     });
   }
