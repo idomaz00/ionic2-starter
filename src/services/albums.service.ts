f1de199efcd711db8ba267fcda0f14de015855ba
Izabela Domazet <idomaz00@fesb.hr>
2017-03-13 11:18:55 +0100

    minor changes

--- src/services/albums.service.ts
+++ src/services/albums.service.ts
@@ -6,35 +6,37 @@ import 'rxjs/add/operator/map';
 import { Album } from '../models/album';
 import { Photo } from '../models/photo';
 
-import { Store } from '@ngrx/store';
-import * as albumsActions from '../actions/albums.action';
+// import { Store } from '@ngrx/store';
+// import * as albumsActions from '../actions/albums.action';
 
-import { AppStore } from '../models/app-store';
+// import { AppStore } from '../models/app-store';
 
 @Injectable()
 export class AlbumsService {
-    albums: Observable<Array<Album>>;
-    photo: Observable<Photo>;
+    // albums: Observable<Array<Album>>;
+    // photo: Observable<Photo>;
     private API_PATH: string = 'https://jsonplaceholder.typicode.com/albums';
     private API_PATH_PHOTOS: string = 'https://jsonplaceholder.typicode.com/photos';
 
-    constructor(private http: Http, private store: Store<AppStore>) {
-        this.albums = store.select('albums');
-        this.photo = store.select('photo');
+    // constructor(private http: Http, private store: Store<AppStore>) {
+    //     this.albums = store.select('albums');
+    //     this.photo = store.select('photo');
+    // }
+
+    constructor(private http: Http) {}
+
+    getAlbums(): Observable<Array<Album>> {
+        return this.http.get(this.API_PATH)
+            .map(res => res.json());
+            //.map(payload => ({ type: albumsActions.ActionTypes.LOAD, payload }))
+            //.subscribe(action => this.store.dispatch(action));
     }
 
-    getAlbums() {
-        this.http.get(this.API_PATH)
+    fetchAlbumPhoto(albumId: number): Observable<Photo> {
+        return this.http.get(`${this.API_PATH_PHOTOS}?albumId=${albumId}&_limit=1`)
             .map(res => res.json())
-            .map(payload => ({ type: albumsActions.ActionTypes.LOAD, payload }))
-            .subscribe(action => this.store.dispatch(action));
-    }
-
-    fetchAlbumPhoto(albumId: number) {
-        this.http.get(`${this.API_PATH_PHOTOS}?albumId=${albumId}&_limit=1`)
-            .map(res => res.json())
-            .map(payload => ({ type: albumsActions.ActionTypes.FETCH_ALBUM_PHOTO, payload }))
-            .subscribe(action => this.store.dispatch(action));
+            // .map(payload => ({ type: albumsActions.ActionTypes.FETCH_ALBUM_PHOTO, payload }))
+            // .subscribe(action => this.store.dispatch(action));
     }
 
 }
